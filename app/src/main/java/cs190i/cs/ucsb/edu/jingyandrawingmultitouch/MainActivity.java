a9fe67d218296b594834cbdbb6f8e89592535859
TheUniqueEye <theuniqueeye@gmail.com>
2017-04-22 01:24:38 -0700

    Initial commit

---
+++ app/src/main/java/cs190i/cs/ucsb/edu/jingyandrawingmultitouch/MainActivity.java
@@ -0,0 +1,53 @@
+package cs190i.cs.ucsb.edu.jingyandrawingmultitouch;
+
+import android.graphics.Canvas;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+
+import static java.lang.Float.valueOf;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d("Finger Painting", "Finger Painting");
+
+        final CanvasView canvasView = (CanvasView) findViewById(R.id.canvas);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                canvasView.clearCanvas();
+            }
+        });
+
+        SeekBar slider = (SeekBar) findViewById(R.id.slider);
+        slider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                canvasView.setBrushSize(progress > 0 ? progress : 1);
+                Log.i("brushSize",progress+"");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+    }
+}
